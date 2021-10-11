26bbcd22f659a4732f13bec76720d7297f068b2c
Christian Nassau <nassau@nullhomotopie.de>
2021-10-11 19:34:18 +0200

    use system Tcl/Tk in the docker image

--- docker-install.sh
+++ docker-install.sh
@@ -3,8 +3,9 @@
 set -e
 
 echo "Installing Yacop Tcl package"
-mkdir $SAGE_LOCAL/lib/yacoptcl || true 
-cp -rf tcl/* $SAGE_LOCAL/lib/yacoptcl 
+TCLLIB=$(echo 'puts [info lib]' | tclsh86) 
+mkdir $TCLLIB/yacoptcl || true 
+cp -rf tcl/* $TCLLIB/yacoptcl 
 
 echo "Installing Yacop Python package"
 sage --python ./setup.py install
